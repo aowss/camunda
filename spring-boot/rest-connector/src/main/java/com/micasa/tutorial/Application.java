249ab5df4e091e34fbe6560f2bb30be589e8386d
Aowss Ibrahim <aowss.ibrahim@bp.com>
2023-12-22 17:15:04 -0500

    Adding the connectors-e2e-test dependencies

--- spring-boot/rest-connector/src/main/java/com/micasa/tutorial/Application.java
+++ spring-boot/rest-connector/src/main/java/com/micasa/tutorial/Application.java
@@ -1,10 +1,17 @@
 package com.micasa.tutorial;
 
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ImportAutoConfiguration({
+        io.camunda.connector.runtime.InboundConnectorsAutoConfiguration.class,
+        io.camunda.connector.runtime.OutboundConnectorsAutoConfiguration.class,
+        io.camunda.connector.runtime.WebhookConnectorAutoConfiguration.class
+})
 @Deployment(resources = "classpath*:*.bpmn")
 public class Application {
     public static void main(String[] args) {
--- spring-boot/rest-connector/src/test/java/com/micasa/tutorial/RESTConnectorProcessTest.java
+++ spring-boot/rest-connector/src/test/java/com/micasa/tutorial/RESTConnectorProcessTest.java
@@ -1,38 +1,41 @@
 package com.micasa.tutorial;
 
-import com.micasa.tutorial.config.BeanConfig;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import com.micasa.tutorial.model.ExchangeRateRequest;
 import com.micasa.tutorial.start.ZeebeController;
-import io.camunda.zeebe.client.ZeebeClient;
-import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 import io.camunda.zeebe.client.api.response.PublishMessageResponse;
-import io.camunda.zeebe.process.test.api.ZeebeTestEngine;
 import io.camunda.zeebe.process.test.assertions.BpmnAssert;
 import io.camunda.zeebe.spring.test.ZeebeSpringTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import io.camunda.connector.e2e.app.TestConnectorRuntimeApplication;
 
-import static com.micasa.tutorial.Utils.completeUserTask;
-import static com.micasa.tutorial.Utils.startProcess;
-import static io.camunda.zeebe.process.test.assertions.BpmnAssert.assertThat;
-import static io.camunda.zeebe.spring.test.ZeebeTestThreadSupport.waitForProcessInstanceCompleted;
-
-@SpringBootTest
+@SpringBootTest(
+    classes = { TestConnectorRuntimeApplication.class, Application.class },
+    properties = { "spring.main.allow-bean-definition-overriding=true", "camunda.connector.polling.enabled=true" },
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 @ZeebeSpringTest
 @DisplayName("REST Connector Test")
 class RESTConnectorProcessTest {
 
-    @Autowired
-    private ZeebeTestEngine engine;
+    @RegisterExtension
+    WireMockExtension wm = WireMockExtension.newInstance()
+            .options(wireMockConfig().bindAddress("127.0.0.1").port(9999))
+            .proxyMode(true)
+            .build();
 
-    @Autowired
-    private ZeebeClient client;
+//    @Autowired
+//    private ZeebeTestEngine engine;
+//
+//    @Autowired
+//    private ZeebeClient client;
 
     @Autowired
     private ZeebeController controller;
@@ -40,17 +43,31 @@ class RESTConnectorProcessTest {
     @Test
     @DisplayName("Using the Zeebe Controller")
     void zeebeController() throws Exception {
+        wm.stubFor(
+            get("/api")
+                .withHost(equalTo("prod-rates"))
+                .willReturn(ok())
+        );
+
         PublishMessageResponse response = controller.startProcess(new ExchangeRateRequest("USD", "CAD", 1000));
+
         BpmnAssert.assertThat(response)
                 .hasCreatedProcessInstance()
                 .extractingProcessInstance()
-//                .hasPassedElementsInOrder(REQUEST_REVIEW, MERGE_CODE, DEPLOY_SNAPSHOT)
+                .hasPassedElement("Start_ExchangeRateRequest")
+                .hasVariableWithValue("apiURL", "http://prod-rates:9999/api")
+                .hasVariableWithValue("toCurrency", "CAD")
+                .hasVariableWithValue("fromCurrency", "USD")
+                .hasVariableWithValue("fromAmount", 1000)
+                .isWaitingAtElements("Task_CallExchangeRateAPI")
+//                .hasAnyIncidents()
+                .isActive();
 //                .hasNotPassedElement(REMIND_REVIEWER)
 //                .hasNotPassedElement(MAKE_CHANGES)
 //                .hasVariableWithValue(REVIEW_RESULT_VAR, "approved")
 //                .extractingLatestCalledProcess()
 //                .hasPassedElement(AUTOMATED_TESTS_RUN_TESTS, 3)
-                .isCompleted();
+//                .isCompleted();
     }
 
 }
