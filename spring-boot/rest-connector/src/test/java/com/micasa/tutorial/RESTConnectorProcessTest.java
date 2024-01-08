261e3b18b9d1d42c9cc6c5a365a982c4cf2620ef
Aowss Ibrahim <aowss.ibrahim@bp.com>
2024-01-08 14:43:31 -0500

    Add error handling to the REST connector sample

--- spring-boot/rest-connector/src/test/java/com/micasa/tutorial/RESTConnectorProcessTest.java
+++ spring-boot/rest-connector/src/test/java/com/micasa/tutorial/RESTConnectorProcessTest.java
@@ -3,6 +3,7 @@ package com.micasa.tutorial;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.micasa.tutorial.model.ExchangeRateRequest;
 import com.micasa.tutorial.start.ZeebeController;
+import io.camunda.zeebe.process.test.api.ZeebeTestEngine;
 import io.camunda.zeebe.process.test.assertions.BpmnAssert;
 import io.camunda.zeebe.process.test.inspections.InspectionUtility;
 import io.camunda.zeebe.process.test.inspections.model.InspectedProcessInstance;
@@ -17,6 +18,9 @@ import static io.camunda.zeebe.spring.test.ZeebeTestThreadSupport.waitForProcess
 
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
 @SpringBootTest
 @ZeebeSpringTest
 @WireMockTest(httpPort = 9999)
@@ -25,11 +29,14 @@ import org.springframework.test.context.ActiveProfiles;
 class RESTConnectorProcessTest {
 
     @Autowired
+    private ZeebeTestEngine engine;
+
+    @Autowired
     private ZeebeController controller;
 
     @Test
     @DisplayName("Using the Zeebe Controller")
-    void zeebeController() throws Exception {
+    void zeebeController() {
         stubFor(
             get(urlPathEqualTo("/exchangeRates"))
                 .withQueryParam("fromCurrency", equalTo("USD"))
@@ -67,6 +74,59 @@ class RESTConnectorProcessTest {
                 .hasVariableWithValue("fromCurrency", "USD")
                 .hasVariableWithValue("fromAmount", 1000)
                 .hasVariableWithValue("exchangeRate", 1.32466);
+    }
+
+    @Test
+    @DisplayName("API error -> default rate")
+    void error() {
+        stubFor(
+            get(urlPathEqualTo("/exchangeRates"))
+                .withQueryParam("fromCurrency", equalTo("INVALID"))
+                .withQueryParam("toCurrency", equalTo("CAD"))
+                .withQueryParam("amount", equalTo("1000"))
+            .willReturn(serverError())
+        );
+
+        controller.startProcess(new ExchangeRateRequest("INVALID", "CAD", 1000));
+
+        InspectedProcessInstance processInstance = InspectionUtility
+                .findProcessInstances()
+                .withBpmnProcessId("Process_RESTConnector")
+                .findFirstProcessInstance()
+                .get();
+
+        waitForProcessInstanceCompleted(processInstance);
+
+        BpmnAssert.assertThat(processInstance)
+                .hasPassedElement("Task-DefaultRate")
+                .hasVariableWithValue("fromCurrency", "INVALID")
+                .hasVariableWithValue("exchangeRate", 1.3); // default rate
+    }
+
+    @Test
+    @DisplayName("Request error -> incident")
+    void incident() throws InterruptedException, TimeoutException {
+        stubFor(
+            get(urlPathEqualTo("/exchangeRates"))
+                .withQueryParam("fromCurrency", equalTo("USD"))
+                .withQueryParam("toCurrency", equalTo("CAD"))
+                .withQueryParam("amount", equalTo("0"))
+            .willReturn(badRequest())
+        );
+
+        controller.startProcess(new ExchangeRateRequest("USD", "CAD", 0));
+
+        InspectedProcessInstance processInstance = InspectionUtility
+                .findProcessInstances()
+                .withBpmnProcessId("Process_RESTConnector")
+                .findFirstProcessInstance()
+                .get();
+
+        engine.waitForBusyState(Duration.ofSeconds(10));
+
+        BpmnAssert.assertThat(processInstance)
+                .hasNotPassedElement("Task_CallExchangeRateAPI")
+                .hasAnyIncidents();
     }
 
 }
